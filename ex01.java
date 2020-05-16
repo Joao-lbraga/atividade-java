374a012340fe914af6404c305cee1a3018f25d7d
Joao-lbraga <58825239+Joao-lbraga@users.noreply.github.com>
2020-05-15 22:20:50 -0300

    3 exercicios  java adicionados

---
+++ ex01.java
@@ -0,0 +1,18 @@
+package ExerciciosJava;
+
+public class ex01 {
+	
+	public static void main (String [] args) {
+		int total = 0;
+		
+		for (int i = 0; i < 500; i = i+3) {
+			if ( i % 2 == 1) {
+				
+				System.out.println(i);
+				total = total + i;
+			}
+		}
+		System.out.println("O total é: " + total );
+	}
+	
+}
---
+++ ex03.java
@@ -0,0 +1,15 @@
+package ExerciciosJava;
+
+public class ex03 {
+	
+	public static void main(String[] args) {
+		for (int i = 233; i < 457; i+=3) {
+			if ( i >299 && i < 401) {
+				i+=2;
+			}
+			System.out.println(i);
+		}
+
+	}
+
+}
