e85b092a23e5e3648cdca207909a04f1b6a0b814
Joao-lbraga <58825239+Joao-lbraga@users.noreply.github.com>
2020-05-15 22:29:43 -0300

    entrega atividade 1 a 3

--- ex03.java
+++ ex03.java
@@ -4,7 +4,7 @@ public class ex03 {
 	
 	public static void main(String[] args) {
 		for (int i = 233; i < 457; i+=3) {
-			if ( i >299 && i < 401) {
+			if ( i > 300 && i < 400) {
 				i+=2;
 			}
 			System.out.println(i);
